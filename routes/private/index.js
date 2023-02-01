d99c3f35118a28d446532c9f09a24c861a127a65
Diksha <dikshabuxani84745@gmail.com>
2023-02-01 16:16:23 +0530

    feat : message apis

--- routes/private/index.js
+++ routes/private/index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const userRoutes = require('./userRoutes');
 const router = express.Router();
 const chatRoutes = require("./chatRoutes");
+const messageRoutes = require("./messageRoutes"); 
 // DEFINE PREFIX OF YOUR PRIVATE ROUTES HERE
 // EXMP : router.use('/ROUTE-NAME', ROUTENAME)
-
+ 
 router.use("/api/user",userRoutes);
 router.use("/api/chat",chatRoutes);
+router.use("/api/message",messageRoutes);
+
+ 
 
 module.exports = router;
